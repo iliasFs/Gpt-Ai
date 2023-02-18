b5b337edac143128d35b0bbb5059ca2e060d5fc9
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-02-18 13:58:53 +0200

    navBar ready -responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
-import{Footer, Blog, Possibility,Features, WhatGPT, Header} from './containers'
-import {Cta, Brand, Navbar} from './Components'
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT,
+  Header,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./Components";
+import "./App.css";
 const App = () => {
-  return <div>Gpt-Ai</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'         //will allow us to hook our application to our index.html
-import ReactDOM from 'react-dom'  //will allow us to hook our application to our index.html
-import App from './App'
+import React from "react"; //will allow us to hook our application to our index.html
+import ReactDOM from "react-dom"; //will allow us to hook our application to our index.html
+import App from "./App";
+import "./index.css";
 
-
-ReactDOM.render(<App />, document.getElementById('root')) // we will render our app to the div with the Id root in index.html
+ReactDOM.render(<App />, document.getElementById("root")); // we will render our app to the div with the Id root in index.html
